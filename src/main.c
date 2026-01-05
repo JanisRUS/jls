579616a02aebdf14ddc1a3ec0dff2a79ffce17bd
JanisRUS <janisrus35@gmail.com>
2026-01-06 00:40:27 +0300

    Добавлена поддержка нескольких аргументов jls

--- src/main.c
+++ src/main.c
@@ -17,66 +17,49 @@ int main(int argc, char *argv[])
 
     setlocale(LC_ALL, "");
 
-    char    *filePtr    = 0;
-    size_t   fileLength = 0;
-
-    filePtr = malloc(2);
-    if (!filePtr)
+    if (argc == 1)
     {
-        isOk = false;
-        goto cleanup;
-    }
-
-    filePtr[fileLength++] = '.';
-    filePtr[fileLength++] = '\0';
-
-    if (argc > 2)
-    {
-        printf("Unsupported argument(s) found. Utility supports only 0 or 1 argument\n");
-        isOk = false;
-        goto cleanup;
-    }
-    if (argc > 1)
-    {
-        char  *lastArgumentPtr = 0;
-
-        lastArgumentPtr = argv[argc - 1];
-
-        free(filePtr);
-        
-        // Для \0
-        filePtr = malloc(strlen(lastArgumentPtr) + 1);
-        if (!filePtr)
+        if (jls(".") != 0)
         {
             isOk = false;
             goto cleanup;
         }
-
-        strcpy(filePtr, lastArgumentPtr);
     }
 
-    if (!fileInfoIsExists(filePtr, &isOk))
+    for (int i = 1; i < argc; ++i)
     {
-        if (isOk)
+        char *filePtr = argv[i];
+
+        if (!fileInfoIsExists(filePtr, &isOk))
         {
-            printf("ls: cannot access '%s': No such file or directory\n", filePtr);
+            if (isOk)
+            {
+                printf("ls: cannot access '%s': No such file or directory\n", filePtr);
+            }
         }
-        goto cleanup;
     }
 
-    if (jls(filePtr) != 0)
+    for (int i = 1; i < argc; ++i)
     {
-        isOk = false;
-        goto cleanup;
+        char *filePtr = argv[i];
+
+        if (!fileInfoSetActiveFile(filePtr))
+        {
+            continue;
+        }
+
+        if (argc > 2 && fileInfoGetType(0) == fileInfoTypeDirectory)
+        {
+            printf("\n%s:\n", filePtr);
+        }
+
+        if (jls(argv[i]) != 0)
+        {
+            isOk = false;
+        }
     }
 
 cleanup:
-    if (filePtr)
-    {
-        free(filePtr);
-        filePtr = 0;
-    }
-
     fileInfoClearActiveFile();
 
     if (isOk)
